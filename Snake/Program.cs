a52f52f66a1e75923fb9c498b2b9af9e45aa7421
EddayfyInWork <46305054+Eddayfy@users.noreply.github.com>
2022-05-28 12:05:40 +0100

    28-05-2022 15:05
    Made some changes here.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -16,7 +16,7 @@ namespace Snake
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new snake());
+            Application.Run(new SnakeForm());
         }
     }
 }
